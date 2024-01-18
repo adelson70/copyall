ea92a2bd1287d4138489b2f9102646782801f7a0
adelson70 <129080230+adelson70@users.noreply.github.com>
2024-01-18 20:26:00 -0300

    mudanças nas funções e botões

--- copyall.py
+++ copyall.py
@@ -1,18 +1,25 @@
-# Importando bibliotecas
 import tkinter as tk
-from tkinter.font import Font
+import ctypes
+import sys
+from tkinter import filedialog, messagebox
 import subprocess
-from tkinter import filedialog
-from tkinter import messagebox
 
-# Função para escolher o caminho da pasta
-# De destino e origem
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if not is_admin():
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    sys.exit()
+
 def escolher_pasta_origem():
     global caminhos
     root = tk.Tk()
     root.withdraw()  # Oculta a janela principal
 
-    pasta_origem = filedialog.askdirectory()
+    pasta_origem = filedialog.askdirectory(title='Selecionar Pasta de Origem')
 
     caminhos.append(pasta_origem)
 
@@ -20,21 +27,20 @@ def escolher_pasta_destino():
     global caminhos
     if len(caminhos) == 0:
         messagebox.showinfo('Pasta de origem não selecionada!', 'Selecione primeiro a pasta de origem dos arquivos!')
-
     else:
         root = tk.Tk()
         root.withdraw()  # Oculta a janela principal
 
-        pasta_destino = filedialog.askdirectory()
+        pasta_destino = filedialog.askdirectory(title='Selecionar Pasta de Destino')
 
         caminhos.append(pasta_destino)
 
 caminhos = []
 
-# Função para fazer a copia dos arquivos
 def copiar():
     global caminhos
-    
+    print(caminhos)
+
     if len(caminhos) == 2:
         caminho_origem = caminhos[0]
         caminho_destino = caminhos[1]
@@ -43,34 +49,30 @@ def copiar():
 
         try:
             resultado = subprocess.run(['powershell', '-Command', comando], capture_output=True, text=True, check=True)
-            print(resultado.stdout)
-        
+            print("Saída padrão:", resultado.stdout)
+
+            if resultado.stderr:  # Exibir mensagem apenas se houver erro
+                messagebox.showinfo('Aviso na cópia', f'Aviso durante a cópia:\n{resultado.stderr}')
+            
         except subprocess.CalledProcessError as e:
-            print(f"Erro ao executar o comando:\n{e.stderr}")
+            messagebox.showinfo('Erro na cópia', f'Erro ao executar o comando:\n{e.stderr}')
 
-
+        caminhos.clear()
+        messagebox.showinfo('Arquivos Copiados', 'Todos os arquivos foram copiados!')
+    else:
+        messagebox.showinfo('Pasta de destino incorreta', 'Escolha uma pasta de destino!')
 
 
 # Criando objeto root
 janela = tk.Tk()
-# Titulo da janela
 janela.title('copyall')
 
-# Label onde usuario ira escolher uma pasta de origem para copiar
-tk.Label(janela, text='Escolha a pasta de origem dos arquivos', font= ..., ).pack(pady=10)
+tk.Label(janela, text='Escolha a pasta de origem dos arquivos').pack(pady=10)
+tk.Button(janela, text='Escolher pasta de origem', command=lambda: escolher_pasta_origem()).pack(pady=10)
 
-# Botão para escolher a pasta de origem dos arquivos
-tk.Button(janela, text='Escolher pasta de origem', font=..., command=lambda: escolher_pasta_origem()).pack(pady=10)
+tk.Label(janela, text='Escolha a pasta de destino para os arquivos copiados').pack(pady=10)
+tk.Button(janela, text='Escolher pasta de destino', command=lambda: escolher_pasta_destino()).pack(pady=10)
 
-# Label onde usuario ira escolher uma pasta de destino para a copia
-tk.Label(janela, text='Escolha a pasta de destino para os arquivos copiados', font= ..., ).pack(pady=10)
-
-# Botão para escolher a pasta de destino dos arquivos
-tk.Button(janela, text='Escolher pasta de origem', font=..., command=lambda: escolher_pasta_destino()).pack(pady=10)
-
-
-# Botão para iniciar ação de copia
-tk.Button(janela, text='copyall!', font=..., command=lambda: escolher_pasta_destino()).pack(pady=10)
-
+tk.Button(janela, text='copyall!', command=lambda: copiar()).pack(pady=10)
 
 janela.mainloop()
