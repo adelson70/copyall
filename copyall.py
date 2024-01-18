01ab3e298ce6d449342d03e53c426b400636ffca
adelson70 <129080230+adelson70@users.noreply.github.com>
2024-01-18 20:07:51 -0300

    add função de copia do powerhell

--- copyall.py
+++ copyall.py
@@ -3,28 +3,51 @@ import tkinter as tk
 from tkinter.font import Font
 import subprocess
 from tkinter import filedialog
+from tkinter import messagebox
 
 # Função para escolher o caminho da pasta
 # De destino e origem
 def escolher_pasta_origem():
+    global caminhos
     root = tk.Tk()
     root.withdraw()  # Oculta a janela principal
 
     pasta_origem = filedialog.askdirectory()
 
-    return pasta_origem
+    caminhos.append(pasta_origem)
 
 def escolher_pasta_destino():
-    root = tk.Tk()
-    root.withdraw()  # Oculta a janela principal
+    global caminhos
+    if len(caminhos) == 0:
+        messagebox.showinfo('Pasta de origem não selecionada!', 'Selecione primeiro a pasta de origem dos arquivos!')
 
-    pasta_destino = filedialog.askdirectory()
+    else:
+        root = tk.Tk()
+        root.withdraw()  # Oculta a janela principal
 
-    return pasta_destino
+        pasta_destino = filedialog.askdirectory()
+
+        caminhos.append(pasta_destino)
+
+caminhos = []
 
 # Função para fazer a copia dos arquivos
 def copiar():
-    ...
+    global caminhos
+    
+    if len(caminhos) == 2:
+        caminho_origem = caminhos[0]
+        caminho_destino = caminhos[1]
+
+        comando = f'robocopy "{caminho_origem}" "{caminho_destino}" /e'
+
+        try:
+            resultado = subprocess.run(['powershell', '-Command', comando], capture_output=True, text=True, check=True)
+            print(resultado.stdout)
+        
+        except subprocess.CalledProcessError as e:
+            print(f"Erro ao executar o comando:\n{e.stderr}")
+
 
 
 
