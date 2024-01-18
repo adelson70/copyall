4348b93d0aba8a4fd90d64596e489d86326d97df
adelson70 <129080230+adelson70@users.noreply.github.com>
2024-01-18 19:50:50 -0300

    add labels

--- copyall.py
+++ copyall.py
@@ -4,10 +4,18 @@ from tkinter.font import Font
 import subprocess
 
 
+# Criando objeto root
 janela = tk.Tk()
+# Titulo da janela
 janela.title('copyall')
 
+# Label onde usuario ira escolher uma pasta de origem para copiar
+tk.Label(janela, text='Escolha a pasta de origem dos arquivos', font= ..., ).pack(pady=10)
 
+
+
+# Label onde usuario ira escolher uma pasta de destino para a copia
+tk.Label(janela, text='Escolha a pasta de destino para os arquivos copiados', font= ..., ).pack(pady=10)
 
 
 janela.mainloop()
